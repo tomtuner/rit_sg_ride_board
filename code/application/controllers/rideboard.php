8e3f9eed78188323b2968cad03b95363a94da074
Dan Fenton <def2191@rit.edu>
2012-11-09 18:04:06 -0500

    created a mock up of home page and created link to login page.

--- code/application/controllers/rideboard.php
+++ code/application/controllers/rideboard.php
@@ -1,6 +1,11 @@
 <?php
 class Rideboard extends CI_Controller {
 
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+	}
+	
 	public function index()
 	{
 		$this->load->view('rideboard_view');
