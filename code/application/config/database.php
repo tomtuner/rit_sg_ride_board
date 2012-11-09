865a74444a5a04bb1bfc4014642f1f6fc1436b4b
Thomas DeMeo <thomasdemeo@gmail.com>
2012-11-09 18:09:46 -0500

    Added configuration parameters for database;

--- code/application/config/database.php
+++ code/application/config/database.php
@@ -49,12 +49,12 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'rideboard';
+$db['default']['password'] = 'rideboard';
+$db['default']['database'] = 'ride_board';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
