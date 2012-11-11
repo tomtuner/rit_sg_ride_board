d3c066236f9c021d6b46f1b150cbcfb7768dba60
Thomas DeMeo <thomasdemeo@gmail.com>
2012-11-11 15:45:14 -0500

    Removed commented out code;

--- code/application/models/signup_model.php
+++ code/application/models/signup_model.php
@@ -8,20 +8,7 @@ class SignUp_model extends CI_Model {
 
 	function register_user($user_array)
 	{
-		/*
-$data = array(
-			'email'	=> $user_array['email'],
-			'password'	=>	$user_array['password'],
-			'first_name'	=>	$user_array['first_name'],
-			'last_name'	=>	$user_array['last_name'],
-			'school_address'	=>	$user_array['school_address'],
-			'home_address'	=>	$user_array['home_address'],
-			'ph_num'	=>	$user_array['ph_num'],
-			'deaf'	=>	$user_array['deaf']
-		);
-*/
 		$this->db->insert('users', $user_array);
-	
 	}
 	
 	function confirm_registration($activation_code)
