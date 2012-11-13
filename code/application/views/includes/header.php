33c36f00c7ea7c8720832604d541ba8ae63f6a5a
Thomas DeMeo <thomasdemeo@gmail.com>
2012-11-12 22:16:54 -0500

    Added nav to header; Initial commit of plan controller and view;

--- code/application/views/includes/header.php
+++ code/application/views/includes/header.php
@@ -1,12 +1,18 @@
-	<div id="header">
-		<div id=access>
-			<ul class="top_nav" >
-				<li class="horizontal"><?= anchor('signup', 'Sign Up','class="right_header_text"'); ?></li>
-				<li class="horizontal"><?= anchor('login', 'Sign In','class="right_header_text"'); ?></li>
-			</ul>
-		</div>
-		<div id="banner">
-			<h1>Welcome to SG Ride Board!</h1>
-		</div>
-		
+<div id="header">
+	<div id=access>
+		<ul class="top_nav" >
+			<li class="horizontal"><?= anchor('signup', 'Sign Up','class="right_header_text"'); ?></li>
+			<li class="horizontal"><?= anchor('login', 'Sign In','class="right_header_text"'); ?></li>
+		</ul>
 	</div>
+	<div id="banner">
+		<h1>Welcome to SG Ride Board!</h1>
+	</div>
+	<div><ul id="list-nav">
+			<li><?= anchor('rideboard', 'Home'); ?></li>
+			<li><?= anchor('explore', 'Explore'); ?></li>
+			<li><?= anchor('plan', 'Plan'); ?></li>
+			<li><?= anchor('about', 'About'); ?></li>
+		</ul>
+	</div>
+</div>
