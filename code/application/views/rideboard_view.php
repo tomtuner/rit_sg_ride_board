c5d1a417f1a37cd857274790acd3ebb61fc26065
Thomas DeMeo <thomasdemeo@gmail.com>
2012-11-23 01:09:12 -0500

    Began revamping the layout/css files;

--- code/application/views/rideboard_view.php
+++ code/application/views/rideboard_view.php
@@ -46,45 +46,5 @@
 			</ul></li>
 
 	</ul>
-	<!--  
-	<ul id="city_list">
-		<li   ><div class="city_text">Springfield, MA</div></li>
-		<li   ><div class="city_text">Saratoga, NY</div>
-									<ul>
-				<li>Sunday</li>
-				<li>Monday</li>
-				<li>Tuesday</li>
-				<li>Wednesday</li>
-				<li>Thursday</li>
-				<li>Friday</li>
-				<li>Saturday</li>
-			</ul></li>
-		<li   ><div class="city_text">Stowe, VT</div></li>
-		<li   ><div class="city_text">New York City, NY</div></li>
-
-	</ul>
-	<ul id="city_list">
-		<li   ><div class="city_text">Boston, MA</div>
-									<ul>
-				<li>Sunday</li>
-				<li>Monday</li>
-				<li>Tuesday</li>
-				<li>Wednesday</li>
-				<li>Thursday</li>
-				<li>Friday</li>
-				<li>Saturday</li>
-			</ul></li>
-		<li   ><div class="city_text">Buffalo, NY</div></li>
-		<li   ><div class="city_text">Burlington, VT</div></li>
-		<li   ><div class="city_text">Syrcuse, NY</div></li>
-
-	</ul>
-	<ul id="city_list">
-		<li   ><div class="city_text">Springfield, MA</div></li>
-		<li   ><div class="city_text">Saratoga, NY</div></li>
-		<li   ><div class="city_text">Stowe, VT</div></li>
-		<li   ><div class="city_text">New York City, NY</div></li>
-
-	</ul>	-->
 </div>
 
